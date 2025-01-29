a8c6e3fc999694235bae91c4adac5fcc4da75bd9
Oliver Calazans <119121726+olivercalazans@users.noreply.github.com>
2025-01-29 18:21:45 -0300

    UNFINISHED: functions to get strings in device descriptions

--- code/api.py
+++ code/api.py
@@ -5,9 +5,10 @@
 
 
 import requests, os, json
-from dotenv           import load_dotenv
-from request_payloads import *
-from display          import Display
+from dotenv            import load_dotenv
+from request_payloads  import *
+from string_extractors import *
+from display           import Display
 
 
 
@@ -21,13 +22,14 @@ class System:
         self._HEADERS    = {"Content-Type": "application/json"}
         self._token      = None
         self._all_hosts  = None
+        self._display    = None
 
 
     def _execute(self) -> None:
         try:
             self._get_authentication_token()
             self._get_all_hosts()
-            self._display_results()
+            self._prepare_data()
         except KeyboardInterrupt:  print(f'{Display.red("Process stopped")}')
         except Exception as error: print(f'{Display.red("Unknown error:")}\n{error}')
 
@@ -43,13 +45,14 @@ class System:
         response        = requests.post(self._ZABBIX_URL, headers=self._HEADERS, data=json.dumps(payload))
         self._all_hosts = response.json()["result"]
 
-
-    def _display_results(self) -> None:
+    
+    def _prepare_data(self) -> None:
         for host in self._all_hosts:
             items = self._get_item_information_of_a_host(host["hostid"])
             if not items or len(items['result']) < 1: continue
-            print(f"\nHost: {host['name']} (ID: {host['hostid']})")
-            print(items['result'][0]['lastvalue'])
+            self._display = {'host': host['name'], 'hostid': host['hostid']}
+            self._get_device_and_software_version()
+            self._display_results()
 
 
     def _get_item_information_of_a_host(self, hostid:str) -> list:
@@ -57,19 +60,18 @@ class System:
         try:
             response = requests.post(self._ZABBIX_URL, headers=self._HEADERS, data=json.dumps(payload))
             response.raise_for_status()
-            result   = response.json()
-            return self._verify_if_there_is_data(result)
+            return response.json()
         except requests.exceptions.RequestException as error:
             raise Exception(f"Error communicating with Zabbix: {error}")
+        
+
+    def _get_device_and_software_version() -> None:
+        ...
 
 
-    @staticmethod
-    def _verify_if_there_is_data(result:list) -> list|None:
-        if "result" in result:
-            return result
-        else:
-            print("Unexpected response from API:", result)
-            return []
+    def _display_results(self) -> None:
+        print(f"\nHost: {self._display['host']} (ID: {self._display['hostid']})")
+
 
 
 
--- code/string_extractors.py
+++ code/string_extractors.py
@@ -4,8 +4,8 @@
 # Permission is hereby granted, free of charge, to any person obtaining a copy of this software...
 
 
-def create_a_dict(device:str, softversion:str) -> dict:
-    return {'device': device, 'softversion': softversion}
+def create_a_dict(device:str, soft_version=None, so=None) -> dict:
+    return {'device': device, 'version': soft_version, 'so': so}
 
 
 def hp_printers(description:str) -> dict:
@@ -15,12 +15,19 @@ def hp_printers(description:str) -> dict:
 
 def hp_laser_string_extractor(description:str) -> dict:
     description = description.split(';')
-    return description[0], description[1].split()[0]
+    return [description[0], description[1].split()[0]]
 
 
 def aruba_switch_string_extractor(description:str) -> dict:
-    description = description.split('Switch')
-    device      = description[0].strip()
-    software    = description[1].split('Version')[1]
-    software    = software.split(',')[0].strip()
-    return device, software
+    description      = description.split(',')[0]
+    device_name      = description.split('Switch')[0].strip()
+    software_version = description.split('Version')[1].strip()
+    return create_a_dict(device_name, software_version)
+
+
+def hp_switch_string_extractor(description:str) -> dict:
+    device_name      = description.split('\n')[1]
+    description      = description.split('\n')[0].split(',')
+    sys_operational  = description[0]
+    software_version = description[1].split()[-1]
+    return create_a_dict(device_name, software_version, sys_operational)
