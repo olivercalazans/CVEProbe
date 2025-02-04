31a51f2f55df5246aec9c8274c93605e89ed28c2
Oliver Calazans <119121726+olivercalazans@users.noreply.github.com>
2025-02-04 14:51:43 -0300

    DRY on main class

--- code/main.py
+++ code/main.py
@@ -11,38 +11,35 @@ from oid              import *
 from display          import Display
 
 
-
 class System:
 
     def __init__(self):
         load_dotenv()
-        self._ZABBIX_URL = os.getenv("ZABBIX_URL")
-        self._HEADERS    = {"Content-Type": "application/json"}
-        self._token      = None
-        self._all_hosts  = None
+        self._all_hosts = None
 
 
     def _execute(self) -> None:
         try:
-            self._get_zabbix_authentication_token()
             self._get_all_hosts_from_zabbix()
             self._prepare_data_obtained_from_zabbix()
+            print(self._all_hosts)
         except KeyboardInterrupt:  print(f'{Display.red("Process stopped")}')
         except Exception as error: print(f'{Display.red("Unknown error:")}\n{error}')
 
 
-    def _get_zabbix_authentication_token(self) -> None:
-        payload     = token_request_payload(os.getenv("USER"), os.getenv("PASSWORD"))
-        response    = requests.post(self._ZABBIX_URL, headers=self._HEADERS, data=json.dumps(payload))
-        self._token = response.json()["result"]
-
-
     def _get_all_hosts_from_zabbix(self) -> None:
-        payload         = device_names_and_ip_payload(self._token)
-        response        = requests.post(self._ZABBIX_URL, headers=self._HEADERS, data=json.dumps(payload))
-        self._all_hosts = response.json()["result"]
+        payload         = token_request_payload(os.getenv("USER"), os.getenv("PASSWORD"))
+        token           = self._get_data_from_zabbix(payload)
+        payload         = device_names_and_ip_payload(token)
+        self._all_hosts = self._get_data_from_zabbix(payload)
 
-    
+
+    @staticmethod
+    def _get_data_from_zabbix(payload:dict) -> str | dict:
+        response = requests.post(os.getenv("ZABBIX_URL"), headers={"Content-Type": "application/json"}, data=json.dumps(payload))
+        return response.json()['result']
+
+
     def _prepare_data_obtained_from_zabbix(self) -> None:
         self._all_hosts = [{'name': dev['host'],'ip': dev['interfaces'][0]['ip']} for dev in self._all_hosts]
 
